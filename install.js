261002a035d2dbea6b195c8de6c29055ed8464bd
aznoqmous <paul.landreau931v@gmail.com>
2020-01-21 13:26:46 +0100

    +auto add scripts

--- install.js
+++ install.js
@@ -1,12 +1,13 @@
 const path = require('path')
 const copyfiles = require('copyfiles')
 const process = require('process')
+const npmAddScript = require('npm-add-script')
 
 let dir = path.resolve(__dirname, '.')
 let cwd = process.env.INIT_CWD
 cwd = path.resolve(cwd, '.')
 
-console.log(`Install PAW inside ${cwd}`)
+console.log(`Installing PAW inside ${cwd}`)
 copyfiles([
   `./manifest.json`,
   `./paw.config.js`,
@@ -15,3 +16,9 @@ copyfiles([
   `./src/*/*`,
   `${cwd}/`
 ], '', ()=>{ console.log('PAW Installation completed') })
+
+process.chdir(cwd)
+npmAddScript({key: 'paw', value: 'webpack --config paw.config.js --mode production'})
+npmAddScript({key: 'paw:dev', value: 'webpack --config paw.config.js --mode development'})
+npmAddScript({key: 'paw:watch', value: 'webpack --config paw.config.js --mode development --watch'})
+console.log(`PAW scripts has been added inside ${cwd}/package.json`)
