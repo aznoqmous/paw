97d4f88a79d8a90de474c2a23ce9500a4df6e81f
aznoqmous <paul.landreau931v@gmail.com>
2020-01-21 10:55:26 +0100

    test postinstall

---
+++ install.js
@@ -0,0 +1,7 @@
+const path = require('path')
+const copy = require('copy')
+
+let cwd = path.resolve(__dirname, '.')
+console.log('dirname:', __dirname)
+console.log('filename:', __filename)
+console.log('cwd:', cwd)
