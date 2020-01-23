679852e3dd1ef712172de2c4afab56fef9e44920
aznoqmous <paul.landreau931v@gmail.com>
2020-01-23 13:59:25 +0100

    re copyfiles with dirpath

--- install.js
+++ install.js
@@ -10,18 +10,18 @@ cwd = path.resolve(cwd, '.')
 console.log(`Installing PAW`)
 console.log(`From : ${dir}`)
 console.log(`To: ${cwd}`)
-// copyfiles([
-//   `./manifest.json`,
-//   `./paw.config.js`,
-//   `./icon-*.png`,
-//   `./src/*`,
-//   `./src/*/*`,
-//   `${cwd}/`
-// ], '', ()=>{ console.log('PAW Installation completed') })
-//
-//
-// process.chdir(cwd)
-// npmAddScript({key: 'paw', value: 'webpack --config paw.config.js --mode production'})
-// npmAddScript({key: 'paw:dev', value: 'webpack --config paw.config.js --mode development'})
-// npmAddScript({key: 'paw:watch', value: 'webpack --config paw.config.js --mode development --watch'})
-// console.log(`PAW scripts has been added inside ${cwd}/package.json`)
+copyfiles([
+  `${dir}/manifest.json`,
+  `${dir}/paw.config.js`,
+  `${dir}/icon-*.png`,
+  `${dir}/src/*`,
+  `${dir}/src/*/*`,
+  `${cwd}/`
+], '', ()=>{ console.log('PAW Installation completed') })
+
+
+process.chdir(cwd)
+npmAddScript({key: 'paw', value: 'webpack --config paw.config.js --mode production'})
+npmAddScript({key: 'paw:dev', value: 'webpack --config paw.config.js --mode development'})
+npmAddScript({key: 'paw:watch', value: 'webpack --config paw.config.js --mode development --watch'})
+console.log(`PAW scripts has been added inside ${cwd}/package.json`)
