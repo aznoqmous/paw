3f128512055e770b816393910629d9433873c3e0
aznoqmous <paul.landreau931v@gmail.com>
2020-01-23 14:22:40 +0100

    working install

--- install.js
+++ install.js
@@ -7,15 +7,14 @@ let dir = path.resolve(__dirname, '.')
 let cwd = process.env.INIT_CWD
 cwd = path.resolve(cwd, '.')
 
-console.log(`Installing PAW`)
-console.log(`From : ${dir}`)
-console.log(`To: ${cwd}`)
+console.log(`Installing PAW...`)
+
 copyfiles([
-  `${dir}/manifest.json`,
-  `${dir}/paw.config.js`,
-  `${dir}/icon-*.png`,
-  `${dir}/src/*`,
-  `${dir}/src/*/*`,
+  './manifest.json',
+  './paw.config.js',
+  './icon-*.png',
+  './src/*',
+  './src/*/*',
   `${cwd}/`
 ], '', ()=>{ console.log('PAW Installation completed') })
 
