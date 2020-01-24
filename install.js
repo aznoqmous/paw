3f511a1dd4923a1ea6fb0a34ccc81edf9dce747b
aznoqmous <paul.landreau931v@gmail.com>
2020-01-24 10:43:03 +0100

    added installedPath to scripts

--- install.js
+++ install.js
@@ -5,6 +5,7 @@ const npmAddScript = require('npm-add-script')
 
 let dir = path.resolve(__dirname, '.')
 let cwd = process.env.INIT_CWD
+let installedPath = "./node_modules/paw"
 cwd = path.resolve(cwd, '.')
 
 console.log(`Installing PAW...`)
@@ -20,10 +21,10 @@ copyfiles([
 ], '', ()=>{
   console.log('PAW Installation completed')
   process.chdir(cwd)
-  npmAddScript({key: 'paw', value: 'node build-manifest.json && webpack --config paw.config.js --mode production'})
-  npmAddScript({key: 'paw:dev', value: 'node build-manifest.json && webpack --config paw.config.js --mode development'})
-  npmAddScript({key: 'paw:watch', value: 'node build-manifest.json && webpack --config paw.config.js --mode development --watch'})
-  npmAddScript({key: 'paw:config', value: 'node setup-config.js && node build-manifest.js'})
-  npmAddScript({key: 'paw:manifest', value: 'node build-manifest.js'})
+  npmAddScript({key: 'paw', value: `node ${installedPath}/build-manifest.json && webpack --config paw.config.js --mode production`})
+  npmAddScript({key: 'paw:dev', value: `node ${installedPath}/build-manifest.json && webpack --config paw.config.js --mode development`})
+  npmAddScript({key: 'paw:watch', value: `node ${installedPath}/build-manifest.json && webpack --config paw.config.js --mode development --watch`})
+  npmAddScript({key: 'paw:config', value: `node ${installedPath}/setup-config.js && node ${installedPath}/build-manifest.js`})
+  npmAddScript({key: 'paw:manifest', value: `node ${installedPath}/build-manifest.js`})
   console.log(`PAW scripts has been added inside ${cwd}/package.json`)
 })
