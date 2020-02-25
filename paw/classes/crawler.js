802f33bbb875f5e26c12e100b3a2665b1ab76f61
aznoqmous <paul.landreau931v@gmail.com>
2020-02-25 17:06:26 +0100

    crawler++

--- paw/classes/crawler.js
+++ paw/classes/crawler.js
@@ -1,6 +1,6 @@
 export default class Crawler {
 
-    constructor(host, config){
+    constructor(host, config) {
         config = Object.assign({
             host: host,
             url: null,
@@ -15,65 +15,71 @@ export default class Crawler {
             bgFetch: null,
 
             //callbacks
-            onNewUrl: null
+            onNewUrl: null,
+
+            allowedExtensions: ['css', 'js', 'jpg', 'jpeg', 'png', 'html', 'php', 'icon', 'json', 'pdf']
 
         }, config)
-        for(let key in config) this[key] = config[key]
+        for (let key in config) this[key] = config[key]
         console.log(this)
     }
 
-    crawlPageAssets(url){
+    crawlPageAssets(url) {
         return this.fetch(url)
-        .then((text)=>{
-            this.newPages([url])
-            let links = this.extractLinks(text)
-            this.newAssets(links.assets)
-            return links.assets
-        })
+            .then((text) => {
+                this.newPages([url])
+                let links = this.extractLinks(text)
+                this.newAssets(links.assets)
+                return links.assets
+            })
     }
 
     // full crawl - stop when no new url if found
-    crawl(url){
+    crawl(url) {
         return this.fetch(url)
-        .then((text)=>{
-            let links = this.extractLinks(text)
+            .then((text) => {
+                let links = this.extractLinks(text)
 
-            this.newAssets(links.assets)
-            let pages = this.newPages(links.pages)
+                this.newAssets(links.assets)
+                let pages = this.newPages(links.pages)
 
-            return Promise.allSettled(pages.map(a => {
-                return this.crawl(a)
-            }))
-            .then(()=>{ return {
-                pages: this.pages,
-                assets: this.assets,
-                errors: this.errors
-            } })
-        })
+                return Promise.allSettled(pages.map(a => {
+                    return this.crawl(a)
+                }))
+                    .then(() => {
+                        return {
+                            pages: this.pages,
+                            assets: this.assets,
+                            errors: this.errors
+                        }
+                    })
+            })
     }
 
-    fetch(url){
+    fetch(url) {
         return new Promise((res, rej) => {
             fetch(url)
-            .then(response => {
-                if(!response.ok) rej(response.status)
-                response.text()
-                .then(text => { res(text) })
-            })
-            .catch(err => {
-                this.errors.push(url)
-                rej({
-                    error: err,
-                    url: url
+                .then(response => {
+                    if (!response.ok) rej(response.status)
+                    response.text()
+                        .then(text => {
+                            res(text)
+                        })
                 })
-            })
+                .catch(err => {
+                    this.errors.push(url)
+                    rej({
+                        error: err,
+                        url: url
+                    })
+                })
         })
     }
 
-    extractLinks(text){
-        let pages = text.match(/\<a[^\>]*?href\=(\"|\')[^\"\']*?(\"|\')/g)
-        let lhrefs = text.match(/\<link[^\>]*?href\=(\"|\')[^\"\']*?(\"|\')/g)
-        let srcs = text.match(/src\=(\"|\')[^\"\']*?(\"|\')/g)
+    extractLinks(text) {
+        let pages = text.match(/\<a[^\>]*?href\=[\"|\'][^\"\']*?[\"|\']/g)
+        let lhrefs = text.match(/\<link[^\>]*?href\=[\"|\'][^\"\']*?[\"|\']/g)
+        let srcs = text.match(/src\=[\"|\'][^\"\']*?[\"|\']/g)
 
         pages = (pages) ? pages : []
         lhrefs = (lhrefs) ? lhrefs : []
@@ -85,43 +91,60 @@ export default class Crawler {
             assets: this.getLinksFromMatches(assets)
         }
     }
-    getLinksFromMatches(matches){
-        if(!matches) return []
+
+    getLinksFromMatches(matches) {
+        if (!matches) return []
         let links = []
+
         matches.map(match => {
-            let link = match.match(/(href|src)\=(\"|\')([^\"\']*?)(\"|\')/)
-            if(link.length && link[3]) {
-                link = link[3]
-                if(!link.match(/^http/)) {
-                    if(link[0] != '/') link = `https://${this.host}/${link}`
-                    else link = `https://${this.host}${link}`
+            let link = match.match(/(href|src)\=[\"|\']([^\"\']*?)[\"|\']/)
+
+            if (link.length && link[2]) {
+                link = link[2]
+
+                if (!/^http/.test(link)) {
+                    let ext = link.match(/[^\/]*?[\.]([[a-z][^\.]]*?)$/)
+                    if(!ext) ext = link.match(/[^\/]*?[\.]([[a-z][^\.]]*?)\//)
+
+                    ext = (ext && ext.length) ? ext[1] : false
+
+                    if(ext && !this.allowedExtensions.includes(ext)) return false;
+
+                    if(/^\//.test(link)) link = `https://${this.host}${link}` // ex: /bloublou.js
+                    else link = `https://${this.host}/${link}` // ex: bloublou.js
                 }
-                if(link.match(/javascript\:history/)) return false
+
+                if (link.match(/javascript\:history/)) return false
+
                 let url = new URL(link)
-                if(url.hostname != this.host) return false
-                if(url) links.push(url.pathname)
+
+                if (url.hostname != this.host) return false
+
+                if (url) links.push(url.pathname)
             }
         })
         return links
     }
-    newPages(pages){
+
+    newPages(pages) {
         let newPages = []
         pages.map(page => {
-            if(!this.pages.includes(page)) {
+            if (!this.pages.includes(page)) {
                 newPages.push(page)
                 this.pages.push(page)
-                if(this.onNewUrl) this.onNewUrl(page, this)
+                if (this.onNewUrl) this.onNewUrl(page, this)
             }
         })
         return newPages
     }
-    newAssets(assets){
+
+    newAssets(assets) {
         let newAssets = []
         assets.map(asset => {
-            if(!this.assets.includes(asset)) {
+            if (!this.assets.includes(asset)) {
                 newAssets.push(asset)
                 this.assets.push(asset)
-                if(this.onNewUrl) this.onNewUrl(asset, this)
+                if (this.onNewUrl) this.onNewUrl(asset, this)
             }
         })
         return newAssets
