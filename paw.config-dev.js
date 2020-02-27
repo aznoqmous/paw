138a120b9f8b8d3a79358c96aa958fb7c80170d2
aznoqmous <paul.landreau931v@gmail.com>
2020-02-27 16:17:38 +0100

    added babel for production build

--- paw.config-dev.js
+++ paw.config-dev.js
@@ -1,26 +1,52 @@
 const path = require('path')
 
 module.exports = [
-  {
-    entry: {
-      path: [
-        './paw/sw-dev.js'
-      ]
+    {
+        entry: {
+            path: [
+                './paw/sw-dev.js'
+            ]
+        },
+        output: {
+            path: path.resolve(__dirname, "."),
+            filename: "sw.js"
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    }
+                }
+            ]
+        }
     },
-    output: {
-      path: path.resolve(__dirname, "."),
-      filename: "sw.js"
+    {
+        entry: {
+            path: [
+                './paw/register-dev.js'
+            ]
+        },
+        output: {
+            path: path.resolve(__dirname, "."),
+            filename: "register.js"
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    }
+                }
+            ]
+        }
     }
-  },
-  {
-    entry: {
-      path: [
-        './paw/register-dev.js'
-      ]
-    },
-    output: {
-      path: path.resolve(__dirname, "."),
-      filename: "register.js"
-    }
-  }
 ]
